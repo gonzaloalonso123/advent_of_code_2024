f02684ceb47852f538b45a0f43363d92d174579b
Gonzalo Alonso Garcia-Amorena <gonzalo@soilbeat.com>
2024-12-04 10:57:28 +0100

    bonus

--- day_2/index.js
+++ day_2/index.js
@@ -9,17 +9,6 @@ const removeIndex = (arr, index) => {
 
 const solve = async (allowedFailures) => {
   const data = await readFile("./data.txt");
-  //   const data = `48 46 47 49 51 54 56`;
-  //     const data = `48 46 47 49 51 54 56
-  //   1 1 2 3 4 5
-  //   1 2 3 4 5 5
-  //   5 1 2 3 4 5
-  //   1 4 3 2 1
-  //   1 6 7 8 9
-  //   1 2 3 4 3
-  //   9 8 7 6 7
-  //   7 10 8 10 11
-  //   29 28 27 25 26 25 22 20`;
   let safeReports = 0;
   data.split("\n").forEach((line, i) => {
     let report = line.split(" ");
