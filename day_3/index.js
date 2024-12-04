0918609e4c4fb4dd97eae5729a17fcf060b35319
Gonzalo Alonso Garcia-Amorena <gonzalo@soilbeat.com>
2024-12-04 12:15:04 +0100

    bonus 3

--- day_3/index.js
+++ day_3/index.js
@@ -1,18 +1,34 @@
 const { readFile } = require("../utils/readFile");
 
-const solve = async () => {
-  const data = await readFile("./data.txt");
-  const regex = /mul\(\d{1,3},\d{1,3}\)/g;
-  const matches = data.match(regex);
-  const clean = matches.map((m) =>
+const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+
+const clean = (matches) =>
+  matches.map((m) =>
     m
       .replace(/[^\d,]/g, "")
       .split(",")
       .map((s) => parseInt(s.trim()))
   );
 
-  const result = clean.reduce((acc, i) => {
-    console.log(i);
+const solve = async () => {
+  const data = await readFile("./data.txt");
+  const matches = data.match(regex);
+  const cleanMatches = clean(matches);
+
+  const result = cleanMatches.reduce((acc, i) => {
+    acc += i[0] * i[1];
+    return acc;
+  }, 0);
+  console.log(result);
+};
+
+const solve2 = async () => {
+  const data = await readFile("./data.txt");
+  const fragments = data.split(/do\(\)/g);
+  const onlyDos = fragments.map((f) => f.split(/don't\(\)/)[0]).join("");
+  const matches = onlyDos.match(regex);
+  const cleanMatches = clean(matches);
+  const result = cleanMatches.reduce((acc, i) => {
     acc += i[0] * i[1];
     return acc;
   }, 0);
@@ -20,3 +36,4 @@ const solve = async () => {
 };
 
 solve();
+solve2();
